3e5640c732d66f4ad0ccb0c094428476779f0c38
ZhxChen <zhxchen6053@outlook.com>
2023-02-16 09:40:36 +0800

    ZhCN

--- data/sql/db-world/trasm_world_texts.sql
+++ data/sql/db-world/trasm_world_texts.sql
@@ -1,29 +1,29 @@
 SET @TEXT_ID := 601083;
 DELETE FROM `npc_text` WHERE `ID` IN  (@TEXT_ID,@TEXT_ID+1);
 INSERT INTO `npc_text` (`ID`, `text0_0`) VALUES
-(@TEXT_ID, 'Transmogrification allows you to change how your items look like without changing the stats of the items.\r\nItems used in transmogrification are no longer refundable, tradeable and are bound to you.\r\nUpdating a menu updates the view and prices.\r\n\r\nNot everything can be transmogrified with each other.\r\nRestrictions include but are not limited to:\r\nOnly armor and weapons can be transmogrified\r\nGuns, bows and crossbows can be transmogrified with each other\r\nFishing poles can not be transmogrified\r\nYou must be able to equip both items used in the process.\r\n\r\nTransmogrifications stay on your items as long as you own them.\r\nIf you try to put the item in guild bank or mail it to someone else, the transmogrification is stripped.\r\n\r\nYou can also remove transmogrifications for free at the transmogrifier.'),
-(@TEXT_ID+1, 'You can save your own transmogrification sets.\r\n\r\nTo save, first you must transmogrify your equipped items.\r\nThen when you go to the set management menu and go to save set menu,\r\nall items you have transmogrified are displayed so you see what you are saving.\r\nIf you think the set is fine, you can click to save the set and name it as you wish.\r\n\r\nTo use a set you can click the saved set in the set management menu and then select use set.\r\nIf the set has a transmogrification for an item that is already transmogrified, the old transmogrification is lost.\r\nNote that the same transmogrification restrictions apply when trying to use a set as in normal transmogrification.\r\n\r\nTo delete a set you can go to the set\'s menu and select delete set.');
+(@TEXT_ID, '幻化功能使你在不改变物品属性的情况下改变物品的外观.\r\n幻化中使用的物品不再是可退款、可交易的，并且将与你绑定.\r\n更新菜单会更新外观和价格.\r\n\r\n不是所有的东西都可以互相幻化.\r\n限制包括但不限于:\r\n只有护甲和武器可以被幻化\r\n枪支、弓和弩可以相互幻化.\r\n钓鱼竿不能幻化.\r\n幻化装备你必须可以使用.\r\n\r\n只有你拥有的装备，幻化才会保留.\r\n如果你试图将装备放入公会银行或邮寄给其他人，那么幻化的效果就会被移除.\r\n\r\n你也可以在幻化师那里免费移除幻化.'),
+(@TEXT_ID+1, '你可以保存你自己的幻化套装.\r\n\r\n要保存，首先你必须将你装备的物品幻化.\r\n然后进入套装管理菜单，选择保存套装,\r\n所有你已经幻化的物品都会显示出来，这样你就能看到你正在保存的物品.\r\n如果你认为这套装备没有问题，你可以点击保存这套装备，并重命名.\r\n\r\n要使用一个套装，你可以在套装管理菜单中点击已保存的套装，然后选择使用套装.\r\n如果该套装对一个已经被幻化的物品进行幻化，那么旧的幻化就会丢失.\r\n请注意，在尝试使用一个套装时，与正常的幻化一样，适用同样的幻化限制.\r\n\r\n要删除一个套装，你可以进入该套装的菜单，选择删除套装.');
 
 SET @STRING_ENTRY := 11100;
 DELETE FROM `acore_string` WHERE `entry` IN  (@STRING_ENTRY+0,@STRING_ENTRY+1,@STRING_ENTRY+2,@STRING_ENTRY+3,@STRING_ENTRY+4,@STRING_ENTRY+5,@STRING_ENTRY+6,@STRING_ENTRY+7,@STRING_ENTRY+8,@STRING_ENTRY+9,@STRING_ENTRY+10, @STRING_ENTRY+11, @STRING_ENTRY+12, @STRING_ENTRY+13, @STRING_ENTRY+14, @STRING_ENTRY+15, @STRING_ENTRY+16);
 INSERT INTO `acore_string` (`entry`, `content_default`) VALUES
-(@STRING_ENTRY+0, 'Item successfully transmogrified.'),
-(@STRING_ENTRY+1, 'Equipment slot is empty.'),
-(@STRING_ENTRY+2, 'Invalid source item selected.'),
-(@STRING_ENTRY+3, 'Source item does not exist.'),
-(@STRING_ENTRY+4, 'Destination item does not exist.'),
-(@STRING_ENTRY+5, 'Selected items are invalid.'),
-(@STRING_ENTRY+6, 'You don''t have enough money.'),
-(@STRING_ENTRY+7, 'You don''t have enough tokens.'),
-(@STRING_ENTRY+8, 'All your transmogrifications were removed.'),
-(@STRING_ENTRY+9, 'No transmogrification found.'),
-(@STRING_ENTRY+10, 'Invalid name inserted.'),
-(@STRING_ENTRY+11, 'Showing transmogrified items, relog to update the current area.'),
-(@STRING_ENTRY+12, 'Hiding transmogrified items, relog to update the current area.'),
-(@STRING_ENTRY+13, 'The selected item is not suitable for transmogrification.'),
-(@STRING_ENTRY+14, 'The selected item cannot be used for transmogrification of the target player.'),
-(@STRING_ENTRY+15, 'Performing transmog appearance sync....'),
-(@STRING_ENTRY+16, 'Appearance sync complete.');
+(@STRING_ENTRY+0, '物品已幻化'),
+(@STRING_ENTRY+1, '装备栏位是空的'),
+(@STRING_ENTRY+2, '所选源装备无效'),
+(@STRING_ENTRY+3, '源装备不存在'),
+(@STRING_ENTRY+4, '目标装备不存在'),
+(@STRING_ENTRY+5, '所选装备无效'),
+(@STRING_ENTRY+6, '金钱不足'),
+(@STRING_ENTRY+7, '代币不足'),
+(@STRING_ENTRY+8, '幻化已取消'),
+(@STRING_ENTRY+9, '目前没有幻化'),
+(@STRING_ENTRY+10, '所插入名称无效'),
+(@STRING_ENTRY+11, '显示幻化物品，重新登录更新当前部位'),
+(@STRING_ENTRY+12, '隐藏幻化物品，重新登录更新当前部位'),
+(@STRING_ENTRY+13, '所选装备不适合幻化'),
+(@STRING_ENTRY+14, '所选装备不能用于目标玩家的幻化'),
+(@STRING_ENTRY+15, '正在执行幻化外观同步...'),
+(@STRING_ENTRY+16, '外观同步完成');
 
 DELETE FROM `command` WHERE `name` IN ('transmog', 'transmog add', 'transmog sync', 'transmog add set');
 INSERT INTO `command` (`name`, `security`, `help`) VALUES
